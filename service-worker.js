3b17ff91de5d2469a2e637ad5eac86b0ba9359b6
monterto <73954649+monterto@users.noreply.github.com>
2026-09-10 16:33:49 -0400

    bugfixes + improvements

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // Bump this on every deploy that changes app files, so old caches get replaced.
-const CACHE_NAME = 'kana-trainer-v16';
+const CACHE_NAME = 'kana-trainer-v1.04';
 
 const APP_SHELL = [
   './',
