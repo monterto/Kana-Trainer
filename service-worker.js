41296194c6d56fcda5e2e03ae72660001a4827ed
monterto <73954649+monterto@users.noreply.github.com>
2026-09-16 00:54:49 -0400

    removed bad description

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // Bump this on every deploy that changes app files, so old caches get replaced.
-const CACHE_NAME = 'kana-trainer-v1.10';
+const CACHE_NAME = 'kana-trainer-v1.11';
 
 const APP_SHELL = [
   './',
